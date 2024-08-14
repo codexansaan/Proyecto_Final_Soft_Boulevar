02c981600f0c05725bf37a23f2a11d11a53a9e51
Andres <ansaan2601@gmail.com>
2024-08-13 23:26:28 -0500

    Creación del proyecto

---
+++ src/main/java/com/boulevar/Soft/Boulevar/SoftBoulevarApplication.java
@@ -0,0 +1,13 @@
+package com.boulevar.Soft.Boulevar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftBoulevarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftBoulevarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boulevar/Soft/Boulevar/SoftBoulevarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boulevar.Soft.Boulevar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftBoulevarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
