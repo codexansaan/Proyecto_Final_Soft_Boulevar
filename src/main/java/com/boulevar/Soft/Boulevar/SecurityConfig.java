248d05ebd92ccb89022d70e99a8434042946752d
Andres <ansaan2601@gmail.com>
2024-08-31 22:44:36 -0500

    Testing de controlador empleado

--- src/main/java/com/boulevar/Soft/Boulevar/SecurityConfig.java
+++ src/main/java/com/boulevar/Soft/Boulevar/SecurityConfig.java
@@ -3,10 +3,16 @@ package com.boulevar.Soft.Boulevar;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -16,14 +22,28 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+
+                .cors(withDefaults())  // Configura CORS antes de las reglas de seguridad
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/empleados/**").permitAll() // Permite acceso sin autenticación a los endpoints
-                        .anyRequest().authenticated() // Requiere autenticación para otras solicitudes
                 )
                 .formLogin(withDefaults()) // Configuración para usar formulario de inicio de sesión por defecto
                 .httpBasic(withDefaults()); // Usa autenticación básica si no se ha configurado otro método
 
         return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:8080");
+        config.addAllowedHeader("*");
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
     @Bean
