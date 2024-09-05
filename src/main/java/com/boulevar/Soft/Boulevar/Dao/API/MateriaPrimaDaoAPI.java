81f298d5e9bf9ca687c6cb3f74ff4444fe17d9c5
KarolBedoya <karsbedoya1@misena.edu.co>
2024-09-05 16:07:05 -0500

    creacion API Controller DaoAPI ServiceApi ServiceImpl de MateriaPrima

---
+++ src/main/java/com/boulevar/Soft/Boulevar/Dao/API/MateriaPrimaDaoAPI.java
@@ -0,0 +1,9 @@
+package com.boulevar.Soft.Boulevar.Dao.API;
+
+import com.boulevar.Soft.Boulevar.models.MateriaPrima;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MateriaPrimaDaoAPI extends CrudRepository<MateriaPrima, Integer> {
+
+
+}
---
+++ src/main/java/com/boulevar/Soft/Boulevar/Service/API/MateriaPrimaServiceAPI.java
@@ -0,0 +1,7 @@
+package com.boulevar.Soft.Boulevar.Service.API;
+
+import com.boulevar.Soft.Boulevar.Commons.GenericServiceAPI;
+import com.boulevar.Soft.Boulevar.models.MateriaPrima;
+
+public interface MateriaPrimaServiceAPI extends GenericServiceAPI<MateriaPrima, Integer> {
+}
